e460196d5618df70b78d94e4586e2afa073c56a2
Diogo Novais <diogo.novais@live.com>
2020-09-02 21:53:46 -0300

    Project created

---
+++ src/test/java/com/novaisdiogo/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novaisdiogo.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
